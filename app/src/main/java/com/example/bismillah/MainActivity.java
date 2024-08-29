e879a80747cd1518960e0bcbfa4f20c627a77679
ferdiodwi <ferdiodwi12@gmail.com>
2024-08-29 21:00:17 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bismillah/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.bismillah;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtEmail, edtPassword;
+    Button btnSbmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtEmail = findViewById(R.id.edt_email);
+        edtPassword = findViewById(R.id.edt_password);
+        btnSbmit = findViewById(R.id.btnsubmit);
+
+
+        btnSbmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = edtEmail.getText().toString();
+                String password = edtPassword.getText().toString();
+
+                Toast.makeText(peekAvailableContext().getApplicationContext(), "Email : " + email + "\nPassword : " + password , Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
